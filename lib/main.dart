7eb9af1c5eefca3d6332d33b78c9bfebe1d5b848
Xenovka <wendyanto18@gmail.com>
2023-08-07 14:49:16 +0700

    applying material3

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter_expense_tracker/widgets/expenses/expenses.dart';
 
 void main() {
   runApp(
-    const MaterialApp(home: Expenses()),
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Expenses(),
+    ),
   );
 }
