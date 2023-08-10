a5c22b7ed3919c90b2850cd2a0c7faa098042fc1
Wendy <wendyanto.bukitvista@gmail.com>
2023-08-10 11:28:13 +0700

    displaying the chart

--- lib/widgets/expenses/expenses.dart
+++ lib/widgets/expenses/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_expense_tracker/widgets/expenses/expenses_list.dart';
+import 'package:flutter_expense_tracker/widgets/chart/chart.dart';
 import 'package:flutter_expense_tracker/widgets/new_expense.dart';
 import 'package:flutter_expense_tracker/models/expense.dart';
 
@@ -87,8 +88,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const SizedBox(height: 10),
-          const Text('The chart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
       ),
